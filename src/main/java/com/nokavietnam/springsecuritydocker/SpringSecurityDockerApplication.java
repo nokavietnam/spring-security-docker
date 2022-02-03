2fa67232870e60d7278578b1ec5c215989080fb8
noka <noka.hieunt@gmail.com>
2022-02-03 11:36:58 +0700

    first commit

---
+++ src/main/java/com/nokavietnam/springsecuritydocker/SpringSecurityDockerApplication.java
@@ -0,0 +1,13 @@
+package com.nokavietnam.springsecuritydocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDockerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringSecurityDockerApplication.class, args);
+  }
+
+}
